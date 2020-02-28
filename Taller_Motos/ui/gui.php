8563660935494d3ccb7294f81088ba59b5b04ad7
Kejebo <kenjen041@gmail.com>
2020-02-28 16:54:57 -0600

    arreglo

--- Taller_Motos/ui/gui.php
+++ Taller_Motos/ui/gui.php
@@ -13,21 +13,14 @@ class Gui
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Mighty motors</title>
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
-<<<<<<< HEAD
-            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-            <link rel="stylesheet" href="assets/css/styles.css">
-             <script src="https://kit.fontawesome.com/b99e675b6e.js">
-
-=======
             <link rel="stylesheet" href="assets/css/styles.css" <script src="https://kit.fontawesome.com/b99e675b6e.js">
-            
+
             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 			<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 			<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 			<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
-        
->>>>>>> 69940853ef9b9020fd35cb4db23f801ceec5ea3f
+
             </script>
 
         </head>
@@ -113,7 +106,7 @@ class Gui
                     </footer>
                 </div>
             </div>
-            
+
             <script src="assets/js/main.js"></script>
             <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css" />
             <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
