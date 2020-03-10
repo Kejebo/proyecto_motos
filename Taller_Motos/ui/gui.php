93c19c93c211479403771e549b4a36067496eccd
Kejebo <kenjen041@gmail.com>
2020-03-10 15:08:56 -0600

    gui

--- Taller_Motos/ui/gui.php
+++ Taller_Motos/ui/gui.php
@@ -13,15 +13,9 @@ class Gui
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Mighty motors</title>
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
-            <link rel="stylesheet" href="assets/css/styles.css" <script src="https://kit.fontawesome.com/b99e675b6e.js">
-
+            <link rel="stylesheet" href="assets/css/styles.css">
+            <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-			<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-			<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-			<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-
-
-            </script>
 
         </head>
 
@@ -39,13 +33,13 @@ class Gui
                         <h2>Sidebar</h2>
                     </div>
                     <ul>
-                        <li><a href="inventario.php"><i class="fas fa-boxes"></i> Inventario</a></li>
+                        <li><a href="inventary.php"><i class="fas fa-boxes"></i> Inventario</a></li>
                         <li><a href="#"><i class="fas fa-tools"></i> Reparacion</a></li>
-                        <li><a href="#"><i class="fas fa-shopping-cart"></i> Compras</a></li>
+                        <li><a href="pucharses.php"><i class="fas fa-shopping-cart"></i> Compras</a></li>
                         <li><a href="ventas.php"><i class="fas fa-cash-register"></i> Ventas</a></li>
                         <li><a href="clients.php"><i class="fas fa-users"></i> Clientes</a></li>
                         <li><a href="proveedores.php"><i class="fas fa-user-tie"></i> Proveedores</a></li>
-                        <li><a href="usuarios.php"><i class="far fa-address-book"></i> Usuarios</a></li>
+                        <li><a href="users.php"><i class="far fa-address-book"></i> Usuarios</a></li>
                         <li><a href="reportes.php"><i class="fas fa-user-cog"></i> Ajustes</a></li>
                     </ul>
 
@@ -67,25 +61,25 @@ class Gui
                             <ul class="navbar-nav mr-auto">
 
                                 <li class="nav-item">
-                                    <a class="nav-link" href="inventario.php">Inventario</a>
+                                    <a class="nav-link" href="inventary.php">Inventario</a>
                                 </li>
                                 <li class="nav-item">
                                     <a class="nav-link" href="#"></i> Reparacion</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="#">Compras</a>
+                                    <a class="nav-link" href="pucharses.php">Compras</a>
                                 </li>
                                 <li class="nav-item">
                                     <a class="nav-link" href="ventas.php">Ventas</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="clientes.php">Clientes</a>
+                                    <a class="nav-link" href="clients.php">Clientes</a>
                                 </li>
                                 <li class="nav-item">
                                     <a class="nav-link" href="proveedores.php">Proveedores</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="usuarios.php">Usuarios</a>
+                                    <a class="nav-link" href="users.php">Usuarios</a>
                                 </li>
                                 <li class="nav-item">
                                     <a class="nav-link" href="reportes.php">Ajustes</a>
@@ -106,7 +100,9 @@ class Gui
                     </footer>
                 </div>
             </div>
-
+            <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
             <script src="assets/js/main.js"></script>
             <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css" />
             <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
