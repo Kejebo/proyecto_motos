76224df451c5a1ac84f2b2bfaa8941d631ef3949
killvibe <57534836+killvibe@users.noreply.github.com>
2020-03-14 21:27:39 -0600

    creacion de clase db_usuarios
    agrege una clase llamada db_usuarios

---
+++ Taller_Motos/db/db_usuario.php
@@ -0,0 +1,53 @@
+<?php 
+require_once('conexion.php');
+
+
+class db_usuarios extends conexion{
+
+
+    function __construct(){
+        parent::__construct();
+    }
+
+    function get_login($data){
+
+        extract($data);
+
+        $sql = "call get_usuario('$correo_electronico','$contrasena','$tipo_usuario')";
+
+        $result = $this->get_data($sql);
+
+        if($result){
+
+            return $result[0];
+
+        }else {
+
+            return false;
+        }
+
+    }
+
+    function insert_usuario($data){
+        $this->conectar();
+        extract($data);
+        $sql = "call insert_usuarios('$nombre','$correo','$tipo','$clave')";
+        $result = $this->execute($sql);
+        return $result;
+        $this->desconectar();
+    }
+
+    function get_ultimo_usuario(){
+        $sql = "call get_ultimo_usuario()";
+        $result = $this->get_data($sql);
+        return $result[0];
+    }
+
+
+}
+
+
+
+
+
+?>
