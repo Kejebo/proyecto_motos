bf7afd3c363af2ef49f04b148c2e77bf8858a58a
killvibe <57534836+killvibe@users.noreply.github.com>
2020-03-15 22:11:50 -0600

    Update index.php
    en el formulario de incio de sesion , le agregue el action hacia security.php

--- Taller_Motos/index.php
+++ Taller_Motos/index.php
@@ -21,13 +21,13 @@ $ui = new GUI($config);
     <div class="col-sm-4"></div>
     
     <div class="col-sm-4">
-    <form action="">
+    <form action="security.php?action=login" method="post">
     <div class="card shadow mt-4">
     <div class="card-header" id="titulosCards">
     <h6 class="card-title" id="nombre_empresa">Taller de Motos Mighty</h6>
     <h5 class="card-title">Bienvenidos</h5>
 
-    <select id="usuarios">
+    <select id="usuarios" name="tipo_usuario">
     <option value="" data-content="<i class='fas fa-users' 'aria-hidden='true'></i>"> Tipos de Usuarios</option>
     <option value="Administrador"><i class="fas fa-user-tie"></i> Administrador</option>
     <option value="Cliente"><a><i class="fas fa-user"></i> Cliente</option>
@@ -37,15 +37,15 @@ $ui = new GUI($config);
     
     <div class="card-body">
     
-    <input type="text" class="form-control" id="inputuser" placeholder="Usuario">
+    <input type="text" class="form-control" id="inputuser" placeholder="Usuario" name="correo_electronico">
     <hr>
 
 
     <div class="input-group mb-3" id="grup-pass">
-    <input type="password" class="form-control pass-input" onblur="quitarborde()" onclick="ponerborde()" placeholder="Contraseña" id=inputpass>
+    <input type="password" class="form-control pass-input" onblur="quitarborde()" onclick="ponerborde()" placeholder="Contraseña" id=inputpass name="contrasena">
     <div class="input-group-append">
     <div id="ojos">
-    <a class="btn" id="ojo" onclick="mostrarPass()"><i onclick="cambiarojo(this)" class="far fa-eye" ></i></a>
+    <a class="btn" id="ojo" onclick="mostrarPass()"><i onclick="cambiarojo('font_ojo')" class="far fa-eye" id="font_ojo"></i></a>
     </div>
     </div>
     </div>
