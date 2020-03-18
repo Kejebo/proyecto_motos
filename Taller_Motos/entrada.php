2f0fe5747e457b627407d809ed0704b5d4602153
killvibe <57534836+killvibe@users.noreply.github.com>
2020-03-18 16:53:54 -0600

    agregue la clase entrada.php
    se agrega la netrada php para test de seguridad

---
+++ Taller_Motos/entrada.php
@@ -0,0 +1,26 @@
+<!-- INICIALIZACIÓN -->
+
+<?php
+
+require_once('ui/gui.php');
+
+$config = array(
+	'titulo'	=> 'Inicio',
+	'url'		=> 'entrada.php',
+);
+
+$ui = new gui($config);
+
+?>
+
+<!-- HEADER -->
+<?php $ui->get_header(); ?>
+
+<h5>Autorizado</h5>
+
+
+<form action="security.php?action=logout" method="post">
+
+	<button class="btn btn-primary">Salir</button>
+
+</form>
