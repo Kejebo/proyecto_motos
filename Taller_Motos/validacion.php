dde425abc92a42fe9a17f3f69789e556f6420a28
killvibe <57534836+killvibe@users.noreply.github.com>
2020-03-23 16:35:06 -0600

    funcionalidades como reenvio de codigo y validacion
    se valida el correo cuando esta incorrecto asi como poder reenviar el codigo en caso de que no haya llegado a la bandeja de correo electronico

--- Taller_Motos/validacion.php
+++ Taller_Motos/validacion.php
@@ -18,9 +18,23 @@ $ui = new gui($config);
 
 <h1>Insertar Codigo</h1>
 
-<form action="security.php?action=validar" method="post">
+<form action="security.php?action=enviar_correo" method="post">
+<h5>Te hemos enviado un codigo de verificacion al correo electronico de tu cuenta Mighty Motors</h5>
+<input type="hidden" value=<?=$_GET['correo'];?> name="correo_electronico_link">
+<h6>Si no te ha llego el codigo prueba <button>reenviar codigo</button></h6>
+</form>
+
+<?php if(isset($_GET['mer'])){?>
+	<div class="alert alert-danger alert-dismissible fade show">
+    <button type="button" class="close" data-dismiss="alert">&times;</button>
+    <strong>Error!</strong>Codigo no valido, Intente nuevamente.
+    </div>
+<?php } ?>
+
+<form action="security.php?action=validar" method="post" id=formulario>
 
 <input type="hidden" value=<?=$_GET['users_id'];?> name="id">
+<input type="hidden" value=<?=$_GET['correo'];?> name="correo_electronico_link">
 
 <input type="text" placeholder="codigo" name="codigo">
 
