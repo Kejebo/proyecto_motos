eeb4fd5aedc2cd2083cf83b97b2859229d138237
killvibe <57534836+killvibe@users.noreply.github.com>
2020-07-17 14:23:34 -0600

    scripts

--- Taller_Motos/ui/gui.php
+++ Taller_Motos/ui/gui.php
@@ -154,14 +154,14 @@ class Gui
             <script src="https://cdn.datatables.net/plug-ins/1.10.20/i18n/Spanish.json"></script>
             
             
-            <script src="https://cdn.datatables.net/buttons/1.6.2/js/dataTables.buttons.min.js"></script>
-            <script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.bootstrap4.min.js"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-            <script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.html5.min.js"></script>
-            <script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.print.min.js"></script>
-            <script src="https://cdn.datatables.net/buttons/1.6.2/js/buttons.colVis.min.js"></script>
+            <script src="assets/js/datatable_buttons.js"></script>
+            <script src="assets/js/datatable_buttons_boot.js"></script>
+            <script src="assets/js/jszip.min.js"></script>
+            <script src="assets/js/pdfmake.min.js"></script>
+            <script src="assets/js/vfs.fonts.js"></script>
+            <script src="assets/js/buttons.html.min.js"></script>
+            <script src="assets/js/buttons.print.min.js"></script>
+            <script src="assets/js/buttons.colVis.min.js"></script>
 
         </body>
 
